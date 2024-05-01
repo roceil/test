55c6be362aa61f7ac5353a7665164465ba9d82bc
roceil <a0978006326@gmail.com>
2024-05-02 00:08:36 +0800

    feat(*): test

--- app.ts
+++ app.ts
@@ -7,6 +7,7 @@ const app: Express = express()
 const port = process.env.PORT ?? 3000
 
 app.get("/", (req: Request, res: Response) => {
+  console.log(req)
   res.send("Express + TypeScript Server")
 })
 
