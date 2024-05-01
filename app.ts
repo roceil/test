e417ae08ffa7342ddb17ae6b0fd6d43a5c83535f
roceil <a0978006326@gmail.com>
2024-05-02 00:21:31 +0800

    feat(*): test

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express, { type Express, type Request, type Response } from "express"
+import { lookup } from "dns"
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -7,11 +8,18 @@ const app: Express = express()
 const port = process.env.PORT ?? 3000
 
 app.get("/", (req: Request, res: Response) => {
-  const ip = req.ip // 客戶端 IP 地址
-  const host = req.headers.host // 客戶端訪問的域名
+  const ip = req.ip as unknown as string // 客戶端 IP 地址
 
-  console.log(`Client IP: ${ip}, Host: ${host}`)
-  res.send(`Your IP address is ${ip} and you are visiting ${host}`)
+  // 執行反向 DNS 查詢
+  lookup(ip, (err, hostname) => {
+    if (err !== null) {
+      console.log(`Reverse lookup failed for IP ${ip}: ${err.message}`)
+      res.send(`Your IP address is ${ip}, but we couldn't determine your hostname.`)
+    } else {
+      console.log(`Client IP: ${ip}, Hostname: ${hostname}`)
+      res.send(`Your IP address is ${ip} and your hostname is ${hostname}`)
+    }
+  })
 })
 
 app.listen(port, () => {
